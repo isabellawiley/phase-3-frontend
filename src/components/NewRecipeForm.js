dcdf4c240cf4bba8a7b386326c7e7ee53f9a8d5a
esellers25 <erin.e.sellers@gmail.com>
2021-05-13 15:51:06 -0400

    form working, shopping list working

--- src/components/NewRecipeForm.js
+++ src/components/NewRecipeForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {useHistory} from "react-router-dom"
+import MultiSelect from "react-multi-select-component";
 
 function NewRecipeForm(){
     const [name, setName] = useState("");
@@ -7,22 +8,22 @@ function NewRecipeForm(){
     const [link, setLink] = useState("");
     const [difficulty, setDifficulty] = useState(null);
     const [description, setDescription] = useState("");
+    // const [produceOne, setProduceOne] = useState("");
+    const [selected, setSelected] = useState([]);
+    // const [produceTwo, setProduceTwo] = useState("");
     const history = useHistory();
+
+    const options = [
+        {label: "Cucumber", value: "Cucumber"},
+        {label: "Kale", value: "Kale"},
+        {label: "Broccoli", value: "Broccoli"},
+        {label: "Tomato", value: "Tomato"},
+    ]
+
+    
 
     function handleSubmit(e){
         e.preventDefault()
-
-        // Promise.all([
-        //     fetch(`http://localhost:9292/producerecipes`, {
-        //         method: "POST",
-        //         headers: {
-        //             "Content-type": "application/json"
-        //         },
-        //         body: JSON.stringify({
-        //             recipe_id: 
-        //         })
-        //     })
-        // ])
 
         fetch(`http://localhost:9292/recipes`, {
             method: "POST",
@@ -34,7 +35,8 @@ function NewRecipeForm(){
                 image: imageURL,
                 link: link,
                 difficulty: difficulty,
-                description: description
+                description: description,
+                produceArr: selected
             })
         })
         .then(res => res.json())
@@ -45,6 +47,7 @@ function NewRecipeForm(){
             setLink("")
             setDifficulty("")
             setDescription("")
+            setSelected([])
         })
 
 
@@ -64,7 +67,29 @@ function NewRecipeForm(){
                 <input className="recipe-data-field" id="difficulty" type="number" min="1" max="5" value={difficulty} onChange={(e) => setDifficulty(e.target.value)}></input>
                 <label htmlFor={"description"} className="recipe-data-field">Recipe Description:</label>
                 <input className="recipe-data-field" id="description" type="text" value={description} onChange={(e) => setDescription(e.target.value)}></input>
+                <label htmlFor="produce-one" className="recipe-data-field">Produce</label>
+                <MultiSelect 
+                options={options}
+                value={selected}
+                onChange={setSelected}
+                labelledBy="Select"
+                />
                 <input className="recipe-data-field" type="submit" value="Add Your Recipe!"></input>
+                {/* <select className="recipe-data-field" id="produce-one" value={produceOne} onChange={(e) => setProduceOne(e.target.value)} name="produce-one">
+                    <option value="">Select Produce</option>
+                    <option value="Cucumber">Cucumber</option>
+                    <option value="Kale">Kale</option>
+                    <option value="Broccoli">Broccoli</option>
+                    <option value="Tomato">Tomato</option>
+                </select> */}
+                {/* <label htmlFor="produce-two" className="recipe-data-field">Produce</label>
+                <select className="recipe-data-field" id="produce-two" value={produceTwo} onChange={(e) => setProduceTwo(e.target.value)} name="produce-two">
+                    <option value="">Select Produce</option>
+                    <option value="Cucumber">Cucumber</option>
+                    <option value="Kale">Kale</option>
+                    <option value="Broccoli">Broccoli</option>
+                    <option value="Tomato">Tomato</option>
+                </select> */}
             </form>
         </div>
     );
--- src/components/ProduceCard.js
+++ src/components/ProduceCard.js
@@ -1,12 +1,26 @@
 
 
 function ProduceCard({produce}){
-    
+    const {name, image, id} = produce
+    function handleAddProduceClick(){
+        fetch(`http://localhost:9292/produces/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                on_list: true
+            })
+        })
+        .then(r => r.json())
+        .then((updatedProduce) => updatedProduce)
+    }
+
     return (
         <div className="card"> 
-            <img src={produce.image} alt={produce.name} className="produce-pic"/>
-            <h3>{produce.name}</h3>
-            <a>See recipe inspirations</a>
+            <img src={image} alt={name} className="produce-pic"/>
+            <h3>{name}</h3>
+            <button onClick={handleAddProduceClick} >Add to Shopping List</button>
         </div>
     );
 }
